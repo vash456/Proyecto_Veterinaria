02dcf1acf73df59aab1b5cb222188c170ba4845a
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-28 15:00:09 -0500

    Update addServices method

--- Servicio.py
+++ Servicio.py
@@ -1,23 +1,13 @@
-from connection import DAO
-
 
 class Servicio:
-    def __init__(self, description, price, idCustomer, dni_veterinarian):
-        self.description = description
-        self.price = price
-        self.idCustomer = idCustomer
-        self.dni_veterinarian = dni_veterinarian
+    def __init__(self, description, price, idCustomer, idVeterinarian):
+        self._description = description
+        self._price = price
+        self._idCustomer = idCustomer
+        self._idVeterinarian = idVeterinarian
 
     def addServices(self):
-        sql = """INSERT INTO services (description, price, idCustomer, DNI_veterinarian) 
-        VALUES ('{0}', {1}, {2}, {3});"""
-        dao = DAO()
-        data = []
-        data.append(self.description)
-        data.append(self.price)
-        data.append(self.idCustomer)
-        data.append(self.dni_veterinarian)
-
-        dao.registerData(data, sql)
-        
-        
+        dict_data = {'description': self._description, 'price': self._price, 'idCustomer': self._idCustomer,
+                     'idVeterinarian': self._idVeterinarian}
+        return dict_data
+                
