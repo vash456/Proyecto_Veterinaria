66d43c08f8d4cfaf3c78ec5fac2db10b09a9c0a8
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-28 14:20:10 -0500

    More updates

--- person.py
+++ person.py
@@ -17,32 +17,32 @@ class Customer(Person):
     def __init__(self, name, last_name, cc, age, tell, email):
         super().__init__(name, last_name, cc, age, tell, email)
         
-    def dic_data(self):
+    def add_customer(self):
         """Create dictionary with customer's information.
 
         Returns:
             dict: Dictionary with customer's information.
         """
-        dic = {'name': self.name, 'last_name': self.last_name, 'cc': self.cc, 'age': self.age,
+        dict_data = {'name': self.name, 'last_name': self.last_name, 'cc': self.cc, 'age': self.age,
                'tell': self.tell, 'email': self.email}
-        return dic
+        return dict_data
 
-class VeterinaryDoctor(Person):
+class Veterinarian(Person):
     """Veterinary employee."""
     def __init__(self, name, last_name, cc, age, tell, email, speciality, salary):
         super().__init__(name, last_name, cc, age, tell, email)
         self.speciality = speciality
         self.salary = salary
         
-    def dic_data(self):
+    def add_veterinarian(self):
         """Create dictionary with veterinarian's information.
 
         Returns:
             dict: Dictionary with veterinarian's information.
         """
-        dic = {'name': self.name, 'last_name': self.last_name, 'cc': self.cc, 'age': self.age,
+        dict_data = {'name': self.name, 'last_name': self.last_name, 'cc': self.cc, 'age': self.age,
                'tell': self.tell, 'email': self.email, 'speciality': self.speciality, 'salary': self.salary}
-        return dic
+        return dict_data
     
     # def give_diagnosis(pet):
     #     pass             
