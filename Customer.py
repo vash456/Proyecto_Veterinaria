53ef8750f6a4fdc082dfef4f71d17cf31865ada6
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-30 00:16:50 -0500

    Private customer attributes issue solved

--- Customer.py
+++ Customer.py
@@ -11,6 +11,6 @@ class Customer(Person):
         Returns:
             dict: Dictionary with customer's information.
         """
-        dict_data = {'name': self.__name, 'last_name': self.__last_name, 'cc': self.__cc, 'age': self.__age,
-               'tell': self.__tell, 'email': self.__email}
+        dict_data = {'name': self._Person__name, 'last_name': self._Person__last_name, 'cc': self._Person__cc,
+                     'age': self._Person__age, 'tell': self._Person__tell, 'email': self._Person__email}
         return dict_data
