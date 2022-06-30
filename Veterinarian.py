e8bbf2e74eacf0834ccc7a05c5418f17be13e3ef
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-30 11:47:29 -0500

    Private attributes issue solved

--- Veterinarian.py
+++ Veterinarian.py
@@ -13,7 +13,8 @@ class Veterinarian(Person):
         Returns:
             dict: Dictionary with veterinarian's information.
         """
-        dict_data = {'name': self.__name, 'last_name': self.__last_name, 'cc': self.__cc, 'age': self.__age,
-               'tell': self.__tell, 'email': self.__email, 'speciality': self.__speciality, 'salary': self.__salary}
+        dict_data = {'name': self._Person__name, 'last_name': self._Person__last_name, 'cc': self._Person__cc,
+                     'age': self._Person__age, 'tell': self._Person__tell, 'email': self._Person__email,
+                     'speciality': self._Veterinarian__speciality, 'salary': self._Veterinarian__salary}
         return dict_data
     
